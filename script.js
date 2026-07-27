5c7a4b21e87ed085eee38c53e4dd14a2f376e315
katariyaresult <gajendrakatariya07@gmail.com>
2026-07-27 11:37:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Katariya Result Premium Loaded");
+const btn=document.getElementById("themeBtn");
+
+btn.onclick=()=>{
+document.body.classList.toggle("dark");
+};
+
+document.body.classList.remove("dark");
