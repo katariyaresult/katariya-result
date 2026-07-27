7d461ebd5a8c04343dcb9df158c347278e7e0e94
katariyaresult <gajendrakatariya07@gmail.com>
2026-07-27 14:26:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -80,3 +80,27 @@ buttons.forEach(function(btn){
 // Hide Top Button Initially
 
 topBtn.style.display="none";
+// Top Button
+
+let topBtn = document.getElementById("topBtn");
+
+window.onscroll = function(){
+
+    if(document.documentElement.scrollTop > 200){
+        topBtn.style.display="block";
+    }
+    else{
+        topBtn.style.display="none";
+    }
+
+};
+
+
+topBtn.onclick = function(){
+
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+
+};
